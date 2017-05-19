4ab869874c130396e807211f3b8ec79f74b569b8
Guus <baggenguus@gmail.com>
2017-05-19 13:59:41 +0200

    initial commit

---
+++ ClipboardDialler.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using System;
+
+class ClipboardDialler
+{
+    [STAThread]
+    public static void Main()
+    {
+        string number = Clipboard.GetText();
+        Call(number);
+    }
+    public static void Call(string number)
+    {
+        Process.Start("callto:" + number);
+    }
+}
+
+
